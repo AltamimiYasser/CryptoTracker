f59c34ee76a603520e413239463d456b438fb51a
yasser tamimi <altamimiy89@gmail.com>
2021-11-16 03:25:18 +0300

    Extension Preview and Beginning of CoinRowView

--- CryptoTracker/Core/Home/Views/CoinRowView.swift.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct CoinRowView_swift: View {
+    let coin: Coin
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+                .frame(minWidth: 30)
+
+            Circle()
+                .frame(width: 30, height: 30)
+
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(.theme.accent)
+
+            Spacer()
+
+            VStack {
+                Text("\(coin.currentPrice)")
+                    .bold()
+                    .foregroundColor(.theme.accent)
+
+                Text("\(coin.priceChangePercentage24H ?? 0)%")
+                    .foregroundColor(// change color if price change is moving up or down
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                        .theme.green:
+                        .theme.red
+                )
+            }
+        }
     }
 }
 
 struct CoinRowView_swift_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView_swift()
+        CoinRowView_swift(coin: dev.coin)
     }
 }
