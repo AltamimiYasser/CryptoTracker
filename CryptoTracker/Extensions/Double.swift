d2ed0ea564e694357f2aa992b58f65eb48573ea9
yasser tamimi <altamimiy89@gmail.com>
2021-11-16 03:40:32 +0300

    Double extension that convert Double to Currency String with 2:6 decimals

---
+++ CryptoTracker/Extensions/Double.swift
@@ -0,0 +1,40 @@
+//
+//  Double.swift
+//  CryptoTracker
+//
+//  Created by Yasser Tamimi on 16/11/2021.
+//
+
+import Foundation
+
+extension Double {
+    
+    /// Convert a `Double` into a currency with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $1234,56
+    /// Convert 12.3456 to $12,3456
+    /// Convert 0.123456 to $0,123456
+    /// ```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "SAR"
+        formatter.currencySymbol = "SR"
+        formatter.minimumIntegerDigits = 2
+        formatter.maximumIntegerDigits = 6
+        return formatter
+    }
+    
+        /// Convert a `Double` into a currency as a `String` with 2-6 decimal places
+        /// ```
+        /// Convert 1234.56 to "$1234,56"
+        /// Convert 12.3456 to "$12,3456"
+        /// Convert 0.123456 to "$0,123456"
+        /// ```
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+}
